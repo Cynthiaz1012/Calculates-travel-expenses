cb14bd0e3bec6d2923b75a01bd8da46b11703a9d
2557705 <2557705@D21026-H0KXM34.VanierCollege.intra>
2026-09-14 10:01:05 -0400

    Create the basic application window

---
+++ src/main/java/com/mycompany/lab04_cynthia/App.java
@@ -0,0 +1,32 @@
+package com.mycompany.lab04_cynthia;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        BorderPane root = new BorderPane();
+        GridPane grid = new GridPane();
+        grid.setPadding(new Insets(20));
+        grid.setHgap(10);
+        grid.setVgap(10);
+        
+        Scene scene = new Scene(root, 500, 600);
+        stage.setTitle("Business Travel Expense Calculator");
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+     public static void main(String[] args) {
+        launch(args);
+    }
+}
